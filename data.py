1d819dbea07d1af10f19caa6d841c81c8889b2f8
alexslcd <155745370+alexslcd@users.noreply.github.com>
2025-06-30 12:06:21 -0500

    actualizacion total

--- data.py
+++ data.py
@@ -1,11 +1,12 @@
+# Ejemplo en tu archivo `data.py`:
 import pymysql.cursors
 
-# Configuración de la conexión a la base de datos
 def get_db_connection():
     return pymysql.connect(
-        host='127.0.0.1',  # Cambia por tu host si es necesario
-        user='alexito',       # Cambia por tu usuario
-        password='salcedo1A',       # Cambia por tu contraseña
-        database='facturacion_db',  # Asegúrate de tener la base de datos creada
+        host='crossover.proxy.rlwy.net',
+        user='root',
+        password='WnkljGqakquedQbSvqVYymprGlijAEGB',
+        database='railway',
+        port=16848,
         cursorclass=pymysql.cursors.DictCursor
     )
